7d42c72d8d047c803e28fe6a3d9fe1f7b71909f1
Gijs Bouwen <gijsbouwen@gmail.com>
2021-03-11 14:01:10 +0100

    renamed unlink_semaphores to close_semaphores

--- philo_two/helper/close_semaphores.c
+++ philo_two/helper/close_semaphores.c
@@ -1,18 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        ::::::::            */
-/*   unlink_semaphores.c                                :+:    :+:            */
+/*   close_semaphores.c                                 :+:    :+:            */
 /*                                                     +:+                    */
 /*   By: gbouwen <marvin@codam.nl>                    +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/03/04 16:11:35 by gbouwen       #+#    #+#                 */
-/*   Updated: 2021/03/11 11:32:51 by gbouwen       ########   odam.nl         */
+/*   Updated: 2021/03/11 14:00:11 by gbouwen       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../philo_two.h"
 
-void	unlink_semaphores(t_data *data)
+void	close_semaphores(t_data *data)
 {
 	sem_close(data->forks);
 	sem_close(data->print_semaphore);
